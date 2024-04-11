535a4e5ce12cb4f9dd3bf146ff3035c13370e73e
Karel Peeters <karel@easics.be>
2024-04-12 00:50:54 +0200

    Add test scripts

---
+++ rust/testm.sh
@@ -0,0 +1,1 @@
+rm -f out.txt && time cargo run --release -- ../data/maarten.invoer | tee out.txt && diff out.txt ../data/maarten.uitvoer
---
+++ rust/testw.sh
@@ -0,0 +1,5 @@
+set -ue
+
+rm -f out.txt && time cargo run --release -- ../data/opgave.invoer | tee out.txt && diff out.txt ../data/opgave.uitvoer
+rm -f out.txt && time cargo run --release -- ../data/voorbeeld.invoer | tee out.txt && diff out.txt ../data/voorbeeld.uitvoer
+rm -f out.txt && time cargo run --release -- ../data/wedstrijd.invoer | tee out.txt && diff out.txt ../data/wedstrijd.uitvoer
